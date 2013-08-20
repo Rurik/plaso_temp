3f999551217a32a1f015a795d07a10affc2103f2
Rurik <brian@thebaskins.com>
2013-08-19 20:12:37 -0400

    Update java_idx.py

--- plaso/parsers/java_idx.py
+++ plaso/parsers/java_idx.py
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" Parser for Java Cache IDX files. """
+"""Parser for Java Cache IDX files."""
 
 # TODO:
 #  * 6.02 files did not retain IP addresses. However, the
@@ -32,11 +32,11 @@ from plaso.lib import timelib
 
 
 class JavaIDXEventContainer(event.EventContainer):
-  """ Convenience class for a Java IDX cache file download container. """
+  """Convenience class for a Java IDX cache file download container."""
   DATA_TYPE = 'java:download:idx'
 
   def __init__(self, idx_version, url, ip_address):
-    """ Initializes the event object.
+    """Initializes the event object.
 
     Args:
       idx_version: Version of IDX file.
@@ -50,8 +50,7 @@ class JavaIDXEventContainer(event.EventContainer):
 
 
 class JavaIDXParser(parser.PlasoParser):
-  """
-  Parse Java IDX files for download events.
+  """Parse Java IDX files for download events.
 
   There are five structures defined. 6.02 files had one generic section
   that retained all data. From 6.03, the file went to a multi-section
@@ -66,7 +65,7 @@ class JavaIDXParser(parser.PlasoParser):
   IDX_SHORT_STRUCT = construct.Struct(
       'magic',
       construct.UBInt8('busy'),
-      construct.UBInt8('inc'),
+      construct.UBInt8('incomplete'),
       construct.UBInt32('idx_version'))
 
   IDX_602_STRUCT = construct.Struct(
@@ -76,12 +75,12 @@ class JavaIDXParser(parser.PlasoParser):
       construct.UBInt32('content_length'),
       construct.UBInt64('last_modified_date'),
       construct.UBInt64('expiration_date'),
-      construct.PascalString('version_string',
-          length_field = construct.UBInt16("length")),
-      construct.PascalString('url',
-          length_field = construct.UBInt16("length")),
-      construct.PascalString('namespace',
-          length_field = construct.UBInt16("length")),
+      construct.PascalString(
+          'version_string', length_field = construct.UBInt16('length')),
+      construct.PascalString(
+          'url', length_field = construct.UBInt16('length')),
+      construct.PascalString(
+          'namespace', length_field = construct.UBInt16('length')),
       construct.UBInt32('FieldCount'))
 
   IDX_605_SECTION_ONE_STRUCT = construct.Struct(
@@ -97,36 +96,41 @@ class JavaIDXParser(parser.PlasoParser):
       construct.UBInt32('sec4len'))
 
   IDX_605_SECTION_TWO_STRUCT = construct.Struct(
-      'Section2',
-      construct.PascalString('version',
-          length_field = construct.UBInt16("length")),
-      construct.PascalString('url',
-          length_field = construct.UBInt16("length")),
-      construct.PascalString('namespec',
-          length_field = construct.UBInt16("length")),
-      construct.PascalString('ip_address',
-          length_field = construct.UBInt16("length")),
+      'IDX_605_Section2',
+      construct.PascalString(
+          'version', length_field = construct.UBInt16('length')),
+      construct.PascalString(
+          'url', length_field = construct.UBInt16('length')),
+      construct.PascalString(
+          'namespec', length_field = construct.UBInt16('length')),
+      construct.PascalString(
+          'ip_address', length_field = construct.UBInt16('length')),
       construct.UBInt32('FieldCount'))
 
   # Java uses Pascal-style strings, but with a 2-byte length field.
   JAVA_READUTF_STRING = construct.Struct(
       'Java.ReadUTF',
-      construct.PascalString('string',
-          length_field = construct.UBInt16("length")))
+      construct.PascalString(
+          'string', length_field = construct.UBInt16('length')))
 
   # Date/time format for HTTP Date field strings.
-  HTTP_DATE_FMT = "%a, %d %b %Y %H:%M:%S %Z"
+  HTTP_DATE_FMT = '%a, %d %b %Y %H:%M:%S %Z'
 
   def Parse(self, file_object):
-    """
-    Extract data from a Java cache IDX file.
+    """Extract data from a Java cache IDX file.
 
     This is the main parsing engine for the parser. It determines if
     the selected file is a proper IDX file. It then checks the file
     version to determine the correct structure to apply to extract
     data.
-    """
 
+    Args:
+      file_object: file_object: A file-like object to read data from.
+    
+    Returns:
+      An EventContainer (JavaIDXEventContainer) with extracted EventObjects
+      that contain the extracted attributes.
+    """
     try:
       magic = self.IDX_SHORT_STRUCT.parse_stream(file_object)
     except IOError as e:
@@ -140,11 +144,11 @@ class JavaIDXParser(parser.PlasoParser):
     # file with other data.
     # Field magic.idx_version is the file version, of which only
     # certain versions are supported.
-    if magic.busy > 1 or magic.inc > 1:
-        raise errors.UnableToParseFile(u'Not a valid Java IDX file')
+    if magic.busy > 1 or magic.incomplete > 1:
+      raise errors.UnableToParseFile(u'Not a valid Java IDX file')
 
     if not magic.idx_version in [602, 603, 604, 605]:
-        raise errors.UnableToParseFile(u'Not a valid Java IDX file')
+      raise errors.UnableToParseFile(u'Not a valid Java IDX file')
 
     # Obtain the relevant values from the file. The last modified date
     # denotes when the file was last modified on the HOST. For example,
@@ -177,8 +181,8 @@ class JavaIDXParser(parser.PlasoParser):
       field = self.JAVA_READUTF_STRING.parse_stream(file_object)
       value = self.JAVA_READUTF_STRING.parse_stream(file_object)
       if field.string == 'date':
-        download_date = timelib.Timestamp.FromTimeString(value.string,
-            self.HTTP_DATE_FMT)
+        download_date = timelib.Timestamp.FromTimeString(
+            value.string, self.HTTP_DATE_FMT)
 
     if not url or not ip_address:
       raise errors.UnableToParseFile(u'Unexpected Error:',
